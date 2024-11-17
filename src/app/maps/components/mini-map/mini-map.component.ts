3290a3fcd086a606fc423320fe2d4a2df665863e
albertost5 <sancheztorresalberto@gmail.com>
2024-11-17 20:11:20 +0100

    Added modules, routing and components

---
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'map-mini-map',
+  templateUrl: './mini-map.component.html',
+  styles: []
+})
+export class MiniMapComponent {
+
+}
